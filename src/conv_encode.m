bb48b09c3d3f4e799f1929c2777e6c06d7ae9a25
maronen <chenxinyao13@mails.tsinghua.edu.cn>
2015-10-28 17:31:57 +0800

    Implement  conv_encode

---
+++ src/conv_encode.m
@@ -0,0 +1,32 @@
+function [ symbols ] = conv_encode( original_symbols, ifHasEnd, efficiency, CRCpoly )
+
+	crc_symbols = crc_encode(original_symbols, CRCpoly, 25*8);
+	crc_symbols = [0; 0; 0; crc_symbols];
+	L = length(crc_symbols);
+	symbols = [];
+
+	if (efficiency == 2)
+		A0 = (1, 1);
+		A1 = (1, 1);
+		A2 = (0, 1);
+		A3 = (1, 1);
+	else
+		A0 = (1, 1, 1);
+		A1 = (0, 1, 1);
+		A2 = (1, 0, 1);
+		A3 = (1, 1, 1);
+	end
+
+	for i = 4: L
+		y = mod(crc_symbols(i)*A0+crc_symbols(i-1)*A1+crc_symbols(i-2)*A2+crc_symbols(i-3)*A3, 2);
+		symbols = [symbols, y];
+	end	
+
+	if (ifHasEnd == 1)
+		symbols = [symbols'; 0; 0; 0];
+	else
+		symbols = symbols';
+	end
+	
+end
+
