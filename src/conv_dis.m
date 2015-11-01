d87584524dea3ba8107133e98b482308f4766eb0
Liudada <liudada2013@outlook.com>
2015-11-02 02:38:11 +0800

    fix decode bugs

--- src/conv_dis.m
+++ src/conv_dis.m
@@ -7,13 +7,13 @@ else
 end
 V = zeros(length(symbols), 1);
 for i = 1:2^efficiency
-	V(i) = exp(j*2*pi/2^efficiency*(i-1));
+	V(i) = exp(1j*2*pi/(2^efficiency)*(i-1));
 end
 
 if hard
 	distances = abs(V - signal);
 	idx = find(distances==min(distances),1);
-	dis = sum(abs(output-symbols(idx)));
+	dis = sum(abs(output-symbols(idx,:)));
 else
 	idx = ismember(symbols,output,'rows');
 	dis = abs(V(idx)-signal);
