b65787205a050c81b6097791af991a5b08052ede
Thomas Lee <lisihan969@gmail.com>
2015-11-03 14:24:17 +0800

    Load dataFile from .mat file instead of generating new one.

--- src/main.m
+++ src/main.m
@@ -2,10 +2,11 @@ close all;
 clear all;
 clc;
 
-dataFile = randi([0, 1], 1024*8, 1); % 1KB
+load 5dBPSNRdata.mat dataFile % 1KB
+% dataFile = randi([0, 1], 1024*8, 1);
 CRC_poly = [1; 1; 0; 0; 0; 0; 0; 0; 0; 1; 1; 1; 1]; % CRC_12: x^12+x^11+x^3+x^2+x+1
 
-PSNR = -5: 1: 15; % dB
+PSNR = -5: 1: 10; % dB
 % block error ratio
 block_err_hard2 = zeros(size(PSNR)); block_err_hard3 = zeros(size(PSNR));
 block_err_soft2 = zeros(size(PSNR)); block_err_soft3 = zeros(size(PSNR));
@@ -24,13 +25,13 @@ for i = 1: length(PSNR)
 	signal_noEnd_2 = conv_send(dataFile, 0, 2, []);
 	signal_3 = conv_send(dataFile, 1, 3, CRC_poly);
 	signal_noCRC_3 = conv_send(dataFile, 1, 3, []);
-	signal_noEnd_3 = conv_send(dataFile, 0, 3, []);		
-	for j = 1: 10		
+	signal_noEnd_3 = conv_send(dataFile, 0, 3, []);
+	for j = 1: 10
 		signal_2n = transmit(signal_2, PSNR(i));
 		signal_3n = transmit(signal_3, PSNR(i));
 		signal_noCRC_2n = transmit(signal_noCRC_2, PSNR(i));
 		signal_noCRC_3n = transmit(signal_noCRC_3, PSNR(i));
-		signal_noEnd_2n = transmit(signal_noEnd_2, PSNR(i));		
+		signal_noEnd_2n = transmit(signal_noEnd_2, PSNR(i));
 		signal_noEnd_3n = transmit(signal_noEnd_3, PSNR(i));
 
 		[file_dec_hard2, block_err] = conv_receive(signal_2n, 1, 2, CRC_poly, 1);
@@ -51,7 +52,7 @@ for i = 1: length(PSNR)
 		if(sum(difference) ~= 0)
 			fail_ratio_hard2(i) = fail_ratio_hard2(i) + 1;
 		end
-		
+
 		[file_dec_noCRC_hard3, ~] = conv_receive(signal_noCRC_3n, 1, 3, [], 1);
 		difference = xor(file_dec_noCRC_hard3, dataFile);
 		error_ratio_hard3(i) = error_ratio_hard3(i) + sum(difference)/length(dataFile);
@@ -65,14 +66,14 @@ for i = 1: length(PSNR)
 		if(sum(difference) ~= 0)
 			fail_ratio_soft2(i) = fail_ratio_soft2(i) + 1;
 		end
-		
+
 		[file_dec_noCRC_soft3, ~] = conv_receive(signal_noCRC_3n, 1, 3, [], 0);
 		difference = xor(file_dec_noCRC_soft3, dataFile);
 		error_ratio_soft3(i) = error_ratio_soft3(i) + sum(difference)/length(dataFile);
 		if(sum(difference) ~= 0)
 			fail_ratio_soft3(i) = fail_ratio_soft3(i) + 1;
 		end
-		
+
 		[file_dec_noEnd_hard2, ~] = conv_receive(signal_noEnd_2n, 0, 2, [], 1);
 		difference = xor(file_dec_noEnd_hard2, dataFile);
 		err_noEnd_hard2(i) = err_noEnd_hard2(i) + sum(difference)/length(dataFile);
@@ -109,7 +110,7 @@ c = jet(4);
 
 figure;
 plot(PSNR, err_noEnd_hard2, 'Color', c(1, :)); hold on;
-plot(PSNR, err_noEnd_hard3, 'Color', c(2, :)); 
+plot(PSNR, err_noEnd_hard3, 'Color', c(2, :));
 xlabel('PSNR(dB)'); ylabel('bit error ratio under hard dicision (no ending)');
 legend('1/2 efficiency', '1/3 efficiency');
 
