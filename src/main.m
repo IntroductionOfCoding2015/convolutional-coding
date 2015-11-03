d25873e7af60cd6bd2f6aa30bc0f87dbf1eb14ef
maronen <chenxinyao13@mails.tsinghua.edu.cn>
2015-11-03 09:39:41 +0800

    Fix some bugs in main, add plot error map

--- src/main.m
+++ src/main.m
@@ -5,7 +5,7 @@ clc;
 dataFile = randi([0, 1], 1024*8, 1); % 1KB
 CRC_poly = [1; 1; 0; 0; 0; 0; 0; 0; 0; 1; 1; 1; 1]; % CRC_12: x^12+x^11+x^3+x^2+x+1
 
-PSNR = -10: 1: 40; % dB
+PSNR = -10: 2.5: 40; % dB
 % block error ratio
 block_err_hard2 = zeros(size(PSNR)); block_err_hard3 = zeros(size(PSNR));
 block_err_soft2 = zeros(size(PSNR)); block_err_soft3 = zeros(size(PSNR));
@@ -25,7 +25,7 @@ for i = 1: length(PSNR)
 	signal_3 = conv_send(dataFile, 1, 3, CRC_poly);
 	signal_noCRC_3 = conv_send(dataFile, 1, 3, []);
 	signal_noEnd_3 = conv_send(dataFile, 0, 3, []);		
-	for j = 1: 100		
+	for j = 1: 10		
 		signal_2n = transmit(signal_2, PSNR(i));
 		signal_3n = transmit(signal_3, PSNR(i));
 		signal_noCRC_2n = transmit(signal_noCRC_2, PSNR(i));
@@ -33,46 +33,46 @@ for i = 1: length(PSNR)
 		signal_noEnd_2n = transmit(signal_noEnd_2, PSNR(i));		
 		signal_noEnd_3n = transmit(signal_noEnd_3, PSNR(i));
 
-		[file_dec_hard2, ~] = conv_receive(signal_2n, 1, 2, CRC_poly, 1);
-		difference = xor(file_dec_hard2, dataFile);
+		[file_dec_hard2, block_err] = conv_receive(signal_2n, 1, 2, CRC_poly, 1);
+		block_err_hard2(i) = block_err_hard2(i) + block_err;
+
+		[file_dec_hard3, block_err] = conv_receive(signal_3n, 1, 3, CRC_poly, 1);
+		block_err_hard3(i) = block_err_hard3(i) + block_err;
+
+		[file_dec_soft2, block_err] = conv_receive(signal_2n, 1, 2, CRC_poly, 0);
+		block_err_soft2(i) = block_err_soft2(i) + block_err;
+
+		[file_dec_soft3, block_err] = conv_receive(signal_3n, 1, 3, CRC_poly, 0);
+		block_err_soft3(i) = block_err_soft3(i) + block_err;
+
+		[file_dec_noCRC_hard2, ~] = conv_receive(signal_noCRC_2n, 1, 2, [], 1);
+		difference = xor(file_dec_noCRC_hard2, dataFile);
 		error_ratio_hard2(i) = error_ratio_hard2(i) + sum(difference)/length(dataFile);
 		if(sum(difference) ~= 0)
 			fail_ratio_hard2(i) = fail_ratio_hard2(i) + 1;
 		end
-
-		[file_dec_hard3, ~] = conv_receive(signal_3n, 1, 3, CRC_poly, 1);
-		difference = xor(file_dec_hard3, dataFile);
+		
+		[file_dec_noCRC_hard3, ~] = conv_receive(signal_noCRC_3n, 1, 3, [], 1);
+		difference = xor(file_dec_noCRC_hard3, dataFile);
 		error_ratio_hard3(i) = error_ratio_hard3(i) + sum(difference)/length(dataFile);
 		if(sum(difference) ~= 0)
 			fail_ratio_hard3(i) = fail_ratio_hard3(i) + 1;
 		end
 
-		[file_dec_soft2, ~] = conv_receive(signal_2n, 1, 2, CRC_poly, 0);
-		difference = xor(file_dec_soft2, dataFile);
+		[file_dec_noCRC_soft2, ~] = conv_receive(signal_noCRC_2n, 1, 2, [], 0);
+		difference = xor(file_dec_noCRC_soft2, dataFile);
 		error_ratio_soft2(i) = error_ratio_soft2(i) + sum(difference)/length(dataFile);
 		if(sum(difference) ~= 0)
 			fail_ratio_soft2(i) = fail_ratio_soft2(i) + 1;
 		end
-
-		[file_dec_soft3, ~] = conv_receive(signal_3n, 1, 3, CRC_poly, 0);
-		difference = xor(file_dec_soft3, dataFile);
+		
+		[file_dec_noCRC_soft3, ~] = conv_receive(signal_noCRC_3n, 1, 3, [], 0);
+		difference = xor(file_dec_noCRC_soft3, dataFile);
 		error_ratio_soft3(i) = error_ratio_soft3(i) + sum(difference)/length(dataFile);
 		if(sum(difference) ~= 0)
 			fail_ratio_soft3(i) = fail_ratio_soft3(i) + 1;
 		end
-
-		[file_dec_noCRC_hard2, block_err] = conv_receive(signal_noCRC_2n, 1, 2, [], 1);
-		block_err_hard2(i) = block_err_hard2(i) + block_err;
-
-		[file_dec_noCRC_hard3, block_err] = conv_receive(signal_noCRC_3n, 1, 3, [], 1);
-		block_err_hard3(i) = block_err_hard3(i) + block_err;
-
-		[file_dec_noCRC_soft2, block_err] = conv_receive(signal_noCRC_2n, 1, 2, [], 0);
-		block_err_soft2(i) = block_err_soft2(i) + block_err;
-
-		[file_dec_noCRC_soft3, block_err] = conv_receive(signal_noCRC_3n, 1, 3, [], 0);
-		block_err_soft3(i) = block_err_soft3(i) + block_err;
-
+		
 		[file_dec_noEnd_hard2, ~] = conv_receive(signal_noEnd_2n, 0, 2, [], 1);
 		difference = xor(file_dec_noEnd_hard2, dataFile);
 		err_noEnd_hard2(i) = err_noEnd_hard2(i) + sum(difference)/length(dataFile);
@@ -83,14 +83,24 @@ for i = 1: length(PSNR)
 
 	end
 
-	% average of 100 times
-	block_err_hard2(i) = block_err_hard2(i)/100; block_err_hard3(i) = block_err_hard3(i)/100;
-	block_err_soft2(i) = block_err_soft2(i)/100; block_err_soft3(i) = block_err_soft3(i)/100;
-	error_ratio_hard2(i) = error_ratio_hard2(i)/100; error_ratio_hard3(i) = error_ratio_hard3(i)/100;
-	error_ratio_soft2(i) = error_ratio_soft2(i)/100; error_ratio_soft3(i) = error_ratio_soft3(i)/100;
-	fail_ratio_hard2(i) = fail_ratio_hard2(i)/100; fail_ratio_hard3(i) = fail_ratio_hard3(i)/100;
-	fail_ratio_soft2(i) = fail_ratio_soft2(i)/100; fail_ratio_soft3(i) = fail_ratio_soft3(i)/100;
-	err_noEnd_hard2(i) = err_noEnd_hard2(i)/100; err_noEnd_hard3(i) = err_noEnd_hard3(i)/100;
+	if(PSNR(i) >=0 && PSNR(i) <= 10)
+		figure;
+		for k = 1: 40
+			dataFile_block = dataFile((k-1)*25*8+1: k*25*8);
+			recFile_block = file_dec_hard2((k-1)*25*8+1: k*25*8);
+			err_code = xor(dataFile_block, recFile_block);
+			subplot(5, 8, k); stem(err_code);
+		end
+	end
+
+	% average of 10 times
+	block_err_hard2(i) = block_err_hard2(i)/10; block_err_hard3(i) = block_err_hard3(i)/10;
+	block_err_soft2(i) = block_err_soft2(i)/10; block_err_soft3(i) = block_err_soft3(i)/10;
+	error_ratio_hard2(i) = error_ratio_hard2(i)/10; error_ratio_hard3(i) = error_ratio_hard3(i)/10;
+	error_ratio_soft2(i) = error_ratio_soft2(i)/10; error_ratio_soft3(i) = error_ratio_soft3(i)/10;
+	fail_ratio_hard2(i) = fail_ratio_hard2(i)/10; fail_ratio_hard3(i) = fail_ratio_hard3(i)/10;
+	fail_ratio_soft2(i) = fail_ratio_soft2(i)/10; fail_ratio_soft3(i) = fail_ratio_soft3(i)/10;
+	err_noEnd_hard2(i) = err_noEnd_hard2(i)/10; err_noEnd_hard3(i) = err_noEnd_hard3(i)/10;
 
 end
 
