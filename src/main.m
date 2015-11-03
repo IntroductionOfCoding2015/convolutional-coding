5ef14fce102d003d0fa68cdcd1f89439645fc3b5
Thomas Lee <lisihan969@gmail.com>
2015-11-03 14:45:07 +0800

    为几个人一起跑数据做准备

--- src/main.m
+++ src/main.m
@@ -2,11 +2,14 @@ close all;
 clear all;
 clc;
 
+INTERATIONS = 10;
+
+
 load 5dBPSNRdata.mat dataFile % 1KB
 % dataFile = randi([0, 1], 1024*8, 1);
 CRC_poly = [1; 1; 0; 0; 0; 0; 0; 0; 0; 1; 1; 1; 1]; % CRC_12: x^12+x^11+x^3+x^2+x+1
 
-PSNR = -5: 1: 10; % dB
+PSNR = -5: 0.5: 10; % dB
 % block error ratio
 block_err_hard2 = zeros(size(PSNR)); block_err_hard3 = zeros(size(PSNR));
 block_err_soft2 = zeros(size(PSNR)); block_err_soft3 = zeros(size(PSNR));
@@ -26,7 +29,7 @@ for i = 1: length(PSNR)
 	signal_3 = conv_send(dataFile, 1, 3, CRC_poly);
 	signal_noCRC_3 = conv_send(dataFile, 1, 3, []);
 	signal_noEnd_3 = conv_send(dataFile, 0, 3, []);
-	for j = 1: 10
+	for j = 1: INTERATIONS
 		signal_2n = transmit(signal_2, PSNR(i));
 		signal_3n = transmit(signal_3, PSNR(i));
 		signal_noCRC_2n = transmit(signal_noCRC_2, PSNR(i));
@@ -95,45 +98,45 @@ for i = 1: length(PSNR)
         suptitle(['error map when PSNR = ', num2str(PSNR(i)), 'dB']);
 	end
 
-	% average of 10 times
-	block_err_hard2(i) = block_err_hard2(i)/10; block_err_hard3(i) = block_err_hard3(i)/10;
-	block_err_soft2(i) = block_err_soft2(i)/10; block_err_soft3(i) = block_err_soft3(i)/10;
-	error_ratio_hard2(i) = error_ratio_hard2(i)/10; error_ratio_hard3(i) = error_ratio_hard3(i)/10;
-	error_ratio_soft2(i) = error_ratio_soft2(i)/10; error_ratio_soft3(i) = error_ratio_soft3(i)/10;
-	fail_ratio_hard2(i) = fail_ratio_hard2(i)/10; fail_ratio_hard3(i) = fail_ratio_hard3(i)/10;
-	fail_ratio_soft2(i) = fail_ratio_soft2(i)/10; fail_ratio_soft3(i) = fail_ratio_soft3(i)/10;
-	err_noEnd_hard2(i) = err_noEnd_hard2(i)/10; err_noEnd_hard3(i) = err_noEnd_hard3(i)/10;
+	% average of INTERATIONS times
+	block_err_hard2(i) = block_err_hard2(i)/INTERATIONS; block_err_hard3(i) = block_err_hard3(i)/INTERATIONS;
+	block_err_soft2(i) = block_err_soft2(i)/INTERATIONS; block_err_soft3(i) = block_err_soft3(i)/INTERATIONS;
+	error_ratio_hard2(i) = error_ratio_hard2(i)/INTERATIONS; error_ratio_hard3(i) = error_ratio_hard3(i)/INTERATIONS;
+	error_ratio_soft2(i) = error_ratio_soft2(i)/INTERATIONS; error_ratio_soft3(i) = error_ratio_soft3(i)/INTERATIONS;
+	fail_ratio_hard2(i) = fail_ratio_hard2(i)/INTERATIONS; fail_ratio_hard3(i) = fail_ratio_hard3(i)/INTERATIONS;
+	fail_ratio_soft2(i) = fail_ratio_soft2(i)/INTERATIONS; fail_ratio_soft3(i) = fail_ratio_soft3(i)/INTERATIONS;
+	err_noEnd_hard2(i) = err_noEnd_hard2(i)/INTERATIONS; err_noEnd_hard3(i) = err_noEnd_hard3(i)/INTERATIONS;
 
 end
 
-c = jet(4);
+% c = jet(4);
 
-figure;
-plot(PSNR, err_noEnd_hard2, 'Color', c(1, :)); hold on;
-plot(PSNR, err_noEnd_hard3, 'Color', c(2, :));
-xlabel('PSNR(dB)'); ylabel('bit error ratio under hard dicision (no ending)');
-legend('1/2 efficiency', '1/3 efficiency');
+% figure;
+% plot(PSNR, err_noEnd_hard2, 'Color', c(1, :)); hold on;
+% plot(PSNR, err_noEnd_hard3, 'Color', c(2, :));
+% xlabel('PSNR(dB)'); ylabel('bit error ratio under hard dicision (no ending)');
+% legend('1/2 efficiency', '1/3 efficiency');
 
-figure;
-plot(PSNR, block_err_hard2, 'Color', c(1, :)); hold on;
-plot(PSNR, block_err_hard3, 'Color', c(2, :)); hold on;
-plot(PSNR, block_err_soft2, 'Color', c(3, :)); hold on;
-plot(PSNR, block_err_soft3, 'Color', c(4, :));
-xlabel('PSNR(dB)'); ylabel('block error ratio');
-legend('1/2, hard dicision', '1/3, hard dicision', '1/2, soft dicision', '1/3, soft dicision');
+% figure;
+% plot(PSNR, block_err_hard2, 'Color', c(1, :)); hold on;
+% plot(PSNR, block_err_hard3, 'Color', c(2, :)); hold on;
+% plot(PSNR, block_err_soft2, 'Color', c(3, :)); hold on;
+% plot(PSNR, block_err_soft3, 'Color', c(4, :));
+% xlabel('PSNR(dB)'); ylabel('block error ratio');
+% legend('1/2, hard dicision', '1/3, hard dicision', '1/2, soft dicision', '1/3, soft dicision');
 
-figure;
-plot(PSNR, error_ratio_hard2, 'Color', c(1, :)); hold on;
-plot(PSNR, error_ratio_hard3, 'Color', c(2, :)); hold on;
-plot(PSNR, error_ratio_soft2, 'Color', c(3, :)); hold on;
-plot(PSNR, error_ratio_soft3, 'Color', c(4, :));
-xlabel('PSNR(dB)'); ylabel('bit error ratio (without CRC)');
-legend('1/2, hard dicision', '1/3, hard dicision', '1/2, soft dicision', '1/3, soft dicision');
+% figure;
+% plot(PSNR, error_ratio_hard2, 'Color', c(1, :)); hold on;
+% plot(PSNR, error_ratio_hard3, 'Color', c(2, :)); hold on;
+% plot(PSNR, error_ratio_soft2, 'Color', c(3, :)); hold on;
+% plot(PSNR, error_ratio_soft3, 'Color', c(4, :));
+% xlabel('PSNR(dB)'); ylabel('bit error ratio (without CRC)');
+% legend('1/2, hard dicision', '1/3, hard dicision', '1/2, soft dicision', '1/3, soft dicision');
 
-figure;
-plot(PSNR, fail_ratio_hard2, 'Color', c(1, :)); hold on;
-plot(PSNR, fail_ratio_hard3, 'Color', c(1, :)); hold on;
-plot(PSNR, fail_ratio_soft2, 'Color', c(1, :)); hold on;
-plot(PSNR, fail_ratio_soft3, 'Color', c(1, :));
-xlabel('PSNR(dB)'); ylabel('fail\_to\_send\_file ratio');
-legend('1/2, hard dicision', '1/3, hard dicision', '1/2, soft dicision', '1/3, soft dicision');
+% figure;
+% plot(PSNR, fail_ratio_hard2, 'Color', c(1, :)); hold on;
+% plot(PSNR, fail_ratio_hard3, 'Color', c(1, :)); hold on;
+% plot(PSNR, fail_ratio_soft2, 'Color', c(1, :)); hold on;
+% plot(PSNR, fail_ratio_soft3, 'Color', c(1, :));
+% xlabel('PSNR(dB)'); ylabel('fail\_to\_send\_file ratio');
+% legend('1/2, hard dicision', '1/3, hard dicision', '1/2, soft dicision', '1/3, soft dicision');
