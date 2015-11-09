c3dc21fba21a02e80e67f81669ee9f5f0fff88bc
Thomas Lee <lisihan969@gmail.com>
2015-11-09 20:43:27 +0800

    Re-implement sym_en/decode

--- src/sym_decode.m
+++ src/sym_decode.m
@@ -1,5 +1,13 @@
 function symbols = sym_decode(signal, efficiency)
-    demodulator = comm.PSKDemodulator(2^efficiency, 'BitOutput', true, ...
-                                                    'PhaseOffset', 0);
-    symbols = step(demodulator, signal);
+    if efficiency == 2
+        gray = [0 1 3 2];
+    else
+        gray = [0 1 3 2 6 7 5 4];
+    end
+
+    levels = 2^efficiency;
+    signal = gray(mod(round(angle(signal) / (2 * pi / levels)), levels) + 1);
+
+    symbols = de2bi(signal, efficiency, 'left-msb')';
+    symbols = symbols(:);
 end
